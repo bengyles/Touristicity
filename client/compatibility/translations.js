1d74440322596a9bfc3c4086889ab38b0f51b026
bengyles <bengyles@hotmail.com>
2017-05-31 21:31:58 +0200

    Improved links to pages (also image instead of just the title)

--- client/compatibility/translations.js
+++
@@ -1,5 +0,0 @@
-angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
-    /* jshint -W100 */
-    gettextCatalog.setStrings('fr', {"test":"testue"});
-    /* jshint +W100 */
-}]);
