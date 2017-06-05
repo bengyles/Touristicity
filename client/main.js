5ab01f04541f3d21150af3fea3db837425a43cb9
bengyles <bengyles@hotmail.com>
2017-06-05 21:00:14 +0200

    islands

--- client/main.js
+++ client/main.js
@@ -133,6 +133,10 @@ function config($locationProvider, $urlRouterProvider, $stateProvider) {
             url: '/green-tourism',
             templateUrl: 'client/views/pages/greenTourism/index.html'
         })
+        .state('islands', {
+            url: '/islands',
+            templateUrl: 'client/views/pages/greenTourism/islands.html'
+        })
         //for the kids
         .state('kids', {
             url: '/kids',
